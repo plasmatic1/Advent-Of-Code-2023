3d96b0423199c8b18e67ead114c6e21a8d599b8e
Plasmatic <moses@mosesxu.net>
2023-12-02 20:11:19 -0500

    improve runner functionality -- add ability to only run sample

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 Advent of Code 2023, in Rust.
 
+To run individual solutions, use `cargo run` or `cargo build` then `./target/debug/aoc[.exe]`.
+
 ## Project hierarchy
 
 * `lib.rs`: Common libraries used for AoC
 * **Runner** (main binary):
-    * Found in `target/debug/aoc`
-    * Executor for individual solutions.  Use `./aoc <day_no> <task_no>` to execute a specific task.  It will run the program on both sample and full inputs
+    * Found in `target/debug/aoc[.exe]`
+    * Executor for individual solutions.  Run executable for help
 * **Solutions** (binaries in `bin/`):
     * Solutions for individual days.  Each binary is executed with `./dayX <task_no> <input_file_path>`
 * Input files are in `inputs/`, where `dayX.txt` is the full input for that day, while `dayX.s1.txt` and `dayX.s2.txt` are the sample inputs for both tasks 1 and 2 respectively
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,38 @@
+extern crate structopt;
+
 use std::process::exit;
 use std::process::Command;
 
-const USAGE: &str = "Usage: ./main <day_no> <task_no>";
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "aoc", about = "AoC solution runner.")]
+struct Opt {
+    #[structopt(name = "DAY", help = "Day number.  Between 1 and 25 (Dec 1 to 25)")]
+    day: i32,
+
+    #[structopt(name = "TASK", help = "Task number.  Either 1 or 2")]
+    task: i32,
+
+    #[structopt(short, long, help = "Only run with sample input")]
+    sample_only: bool,
+}
 
 fn main() {
-    // get args
-    let args: Vec<String> = std::env::args().collect();
-    let day_no = i32::from_str_radix(&args.get(1).expect(USAGE), 10).expect(USAGE);
-    let task_no = i32::from_str_radix(&args.get(2).expect(USAGE), 10).expect(USAGE);
-    if !(1 <= day_no && day_no <= 25) {
-        println!("Invalid day {}!", day_no);
+    let opt = Opt::from_args();
+    if !(1 <= opt.day && opt.day <= 25) {
+        println!("Invalid day {}!", opt.day);
         exit(1);
     }
-    if !(1 <= task_no && task_no <= 2) {
-        println!("Invalid task number {}! (must be 1 or 2)", task_no);
+    if !(1 <= opt.task && opt.task <= 2) {
+        println!("Invalid task number {}! (must be 1 or 2)", opt.task);
         exit(1);
     }
-    println!("Problem: day {} task {}", day_no, task_no);
+    println!("Problem: day {} task {}", opt.day, opt.task);
 
     // run proc
-    let in_sample_path = format!("inputs/day{}.s{}.txt", day_no, task_no);
-    let in_path = format!("inputs/day{}.txt", day_no);
+    let in_sample_path = format!("inputs/day{}.s{}.txt", opt.day, opt.task);
+    let in_path = format!("inputs/day{}.txt", opt.day);
     println!("Sample input path: {}", &in_sample_path);
     println!("Input path: {}", &in_path);
 
@@ -29,13 +41,13 @@ fn main() {
         "windows" => ".exe",
         _ => ""
     };
-    let soln_path = format!("target/debug/day{}{}", day_no, soln_ext);
+    let soln_path = format!("target/debug/day{}{}", opt.day, soln_ext);
     println!("Solution path: {}", soln_path);
 
     println!();
 
     let run_with_input_file = |path: &str| {
-        match Command::new(&soln_path).arg(&task_no.to_string()).arg(path).output() {
+        match Command::new(&soln_path).arg(&opt.task.to_string()).arg(path).output() {
             Ok(o) => {
                 if !o.stderr.is_empty() {
                     print!("ERR: Program returned error: ");
@@ -58,6 +70,8 @@ fn main() {
     println!("=== Running with sample input ===");
     run_with_input_file(&in_sample_path);
 
-    println!("=== Running with real input ===");
-    run_with_input_file(&in_path);
+    if !opt.sample_only {
+        println!("=== Running with real input ===");
+        run_with_input_file(&in_path);
+    }
 }
